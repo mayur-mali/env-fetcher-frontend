9d64ce1aa3577c6ae832f5e25dee47d9d85d2c66
mayur mali <mayurmali31@gmail.com>
2025-05-29 16:13:54 +0530

    fix login page isuue

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import errorImage from "../assets/404.svg";
 function NotFound() {
-  return <div>Not Found</div>;
+  return (
+    <div className="flex justify-between items-center h-screen p-4">
+      <img
+        src={errorImage}
+        alt="Page Not Found"
+        style={{ width: "100%", height: "70vh", objectFit: "contain" }}
+      />
+    </div>
+  );
 }
 
 export default NotFound;
